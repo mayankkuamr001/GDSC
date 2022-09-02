64205744434cb4c8b21a4c098298b7dc183d2e2f
Mayank Kumar Gupta <mk6445363@gmail.com>
2022-09-02 20:14:09 +0530

    Jokes API created

--- script.js
+++ script.js
@@ -8,4 +8,17 @@ function getQuotes() {
         .then(function (data) {
             quotes.innerHTML = JSON.stringify(data[0].text);
         });
+        getJokes();
+}
+
+function getJokes() {
+    // reference: https://api.chucknorris.io/jokes/random
+    const jokes = document.getElementById("jokes-data");
+    fetch("https://api.chucknorris.io/jokes/random")
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            jokes.innerHTML = JSON.stringify(data.value);
+        });
 }
